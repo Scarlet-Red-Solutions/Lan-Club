3ae4e7e0402263384a1d277f96f9e28c4e92d037
Cuberice <neil.deswardt@gmail.com>
2014-06-11 09:28:42 +0200

    Adding FormTeamRegistration

---
+++ Lan Club Toernooi/FormTeamRegistration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lan_Club_Toernooi
+{
+
+	// define fields to be shown on form Team Registration
+	// form create gaan deur en error nie
+	public partial class FormTeamRegistration : Form
+	{
+		public FormTeamRegistration()
+		{
+			InitializeComponent();
+
+			if (frmMain.pressedButton == "LoL")
+			{
+				pnlPosition.Visible = true;
+				grbGame.Text = "League of Legends";
+			}
+
+			else
+				if (frmMain.pressedButton == "DoTa2")
+				{
+					pnlPosition.Visible = true;
+					grbGame.Text = "DOTA2";
+				}
+
+				else
+				{
+					pnlPosition.Visible = false;
+					grbGame.Text = "Call Of Duty";
+				}
+		}
+
+		private void btnSave_Click(object sender, EventArgs e)
+		{
+			this.Close();
+
+		}
+	}
+}
